0f9b9e34d18ebac2930290e4d6ebc5f2745cda63
mbos2 <mbosnjak2@outlook.com>
2020-06-16 22:00:37 +0200

    Added some world bounds

--- index.ts
+++ index.ts
@@ -5,15 +5,27 @@ import LoadScene from './src/scenes/load';
 
 const config = {
   type: Phaser.AUTO,
-  width: screen.width,
-  height: screen.height,
+  width: 1500,
+  height: 1000,
   scene: [LoadScene, GameScene],
   backgroundColor: '#2d2d2d',
   physics: {
     default: 'arcade',
     arcade: {
+      x: 0,
+      y: 0,
+      // width: this.scene.sys.scale.width,
+      // height: this.scene.sys.scale.height,
       gravity: { y: 300 },
       debug: false,
+      fps: 60,
+      checkCollision: {
+        up: true,
+        down: true,
+        left: true,
+        right: true
+      },
+      timeScale: 0.6, 
     },
   },
 };
